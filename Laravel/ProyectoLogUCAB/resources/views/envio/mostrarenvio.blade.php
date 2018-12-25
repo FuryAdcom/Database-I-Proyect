44f97bc9aaefca0fe560798b40a8f38484cbc099
FuryAdcom <33576475+FuryAdcom@users.noreply.github.com>
2018-12-25 19:15:35 -0400

    Version 4.3.1
    Termometro

--- Laravel/ProyectoLogUCAB/resources/views/envio/mostrarenvio.blade.php
+++ Laravel/ProyectoLogUCAB/resources/views/envio/mostrarenvio.blade.php
@@ -12,27 +12,42 @@
 						<form action="/envio/store3" method="POST">
 							@csrf
 
-							<input class="form-control" name="paquete" type="number" value="{{$request->paquete}}" hidden>
-							<input class="form-control" name="veh_rut" type="number" value="{{$request->Vehiculo}}" hidden>
-							<input class="form-control" name="costo" type="number" value="{{$costo}}" hidden>
-							<input class="form-control" name="destino" type="number" value="{{$destino}}" hidden>
-							<input class="form-control" name="ruta" type="number" value="{{$route}}" hidden>
-							<div class="form-group" style="width:100%; float: left;margin-bottom:0px">
-								<label for="inputPaquete" style="color: whitesmoke;">Paquete a enviar: {{$paquete->Numero_guia}}</label><br>
-								<label for="inputPeso" style="color: whitesmoke;">Peso de paquete: {{$paquete->Peso}}</label><br>
-								<label for="inputDimensiones" style="color: whitesmoke;">Dimensiones del paquete (AxLxP en cm): {{$paquete->Ancho.' x '.$paquete->Largo.' x '.$paquete->Profundidad}}</label><br>
-								<label for="inputOG" style="color: whitesmoke;">Oficina de envio: {{$ofiog->Nombre.'. Codigo: '.$ofiog->Codigo}}</label><br>
-								<label for="inputDEST" style="color: whitesmoke;">Oficina destino: {{$ofidest->Nombre.'. Codigo: '.$ofidest->Codigo}}</label><br>
-								<h1 for="inputCosto" style="color: whitesmoke;">Costo del envio: {{$costo}}</h1><br>
-								<h1 for="inputEstimado" style="color: whitesmoke;">Fecha estimada: {{$est}}</h1><br>
-                            </div>
-                            <h1 style="color: whitesmoke">Si esta seguro de estos datos, presione Enviar.</h1>
 
 							<div style="width:100%; height: 40px; float: left;">
 								<button type="submit" class="btn btn-primary">Enviar</button>
 							</div>
                         </form>
                         <!---->
+
+                        <div id="content">
+
+                                <div id="thermometer">
+                        
+                                    <div class="track">
+                                        <div class="goal">
+                                            <div class="amount"> 800 </div>
+                                        </div>
+                                        <div class="progress">
+                                            <div class="amount">734 </div>
+                                        </div>
+                                        <div class="stop1">
+                                            <div class="amount">$0 </div>
+                                        </div>
+                                        <div class="stop2">
+                                            <div class="amount">$200 </div>
+                                        </div>
+                                        <div class="stop3">
+                                            <div class="amount">$400 </div>
+                                        </div>
+                                        <div class="stop4">
+                                            <div class="amount">$600 </div>
+                                        </div>
+                                    </div>
+                        
+                                </div>
+                        
+                            </div>
+
 					</div>
 				</div>		
 @endsection
